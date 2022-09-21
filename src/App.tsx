20e03b0e500574dee61621f7cb2f0a3bade4a0f7
DoanRii <113299034+DoanRii@users.noreply.github.com>
2022-09-21 12:13:45 +0800

    1 file modified, 2 added, 2 deleted

--- src/App.tsx
+++ src/App.tsx
@@ -77,12 +77,12 @@ function App() {
                   {colorScheme === 'dark' ? (
                     <img
                       style={{ width: '10px', display: 'flex' }}
-                      src="https://imgs.search.brave.com/WpDMAl-TDIbYAs5qx8KOjez8x6I4T5N4DPZxSWY-5mQ/rs:fit:1000:1000:1/g:ce/aHR0cDovL2Nkbi5v/bmxpbmV3ZWJmb250/cy5jb20vc3ZnL2lt/Z18zNTYyNjguc3Zn.svg"
+                      src="./assets/passmanlogo.png"
                     ></img>
                   ) : (
                     <img
                       style={{ width: '10px', display: 'flex' }}
-                      src="https://imgs.search.brave.com/8PB_e8ZjSb6UpmdrkEOlV7-RFvw_1ExWfEPE6wRNfnQ/rs:fit:1200:1200:1/g:ce/aHR0cHM6Ly9zdGF0/aWMudmVjdGVlenku/Y29tL3N5c3RlbS9y/ZXNvdXJjZXMvcHJl/dmlld3MvMDAwLzQy/NC8zMzkvb3JpZ2lu/YWwvbG9nb3V0LWlj/b24tdmVjdG9yLWls/bHVzdHJhdGlvbi5q/cGc"
+                      src="./assets/passmanlogo-dark.png"
                     ></img>
                   )}
                 </Group>
